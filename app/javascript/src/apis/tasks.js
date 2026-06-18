d1ae11fd9f862de49ff57babb98256fdcb87ce3b
chirag <chiragtharad01@gmail.com>
2026-06-18 14:56:36 +0530

    Added ability to update a task

--- app/javascript/src/apis/tasks.js
+++ app/javascript/src/apis/tasks.js
@@ -4,8 +4,11 @@ const fetch = () => axios.get("/tasks");
 
 const show = slug => axios.get(`/tasks/${slug}`);
 
+const update = ({ slug, payload }) =>
+  axios.put(`/tasks/${slug}`, { task: payload });
+
 const create = payload => axios.post("/tasks", { task: payload });
 
-const tasksApi = { fetch, show, create };
+const tasksApi = { fetch, show, update, create };
 
 export default tasksApi;

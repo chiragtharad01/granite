6e45512008f5c935461cec24273d691176a1ed1e
chirag <chiragtharad01@gmail.com>
2026-06-18 13:21:01 +0530

    Added show page for a task

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :tasks, only: %i[index create], param: :slug
+  resources :tasks, only: %i[index create show], param: :slug
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

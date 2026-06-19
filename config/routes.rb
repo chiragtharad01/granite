9c97e8df554240b2ee6ab705ad27a54e208db542
chirag <chiragtharad01@gmail.com>
2026-06-19 12:45:58 +0530

    Added ability to assign task to a user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   resources :tasks, except: %i[new edit], param: :slug
+  resources :users, only: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
